9617e1fbb98479be21c1ff4e3e551b03768f0c52
ACE-The-Dev <22charles.king@gmail.com>
2021-04-09 20:51:06 -0500

    huhm

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
     var per = $this.attr("per");
     $this.css("width", per+"%");
         $({animatedValue: 0}).animate({animatedValue: per},{
-            duration: 1000,
+            duration: 2000,
             step: function(){
                 $this.attr("per",Math.floor(this.animatedValue) + "%");
             },
